c283d3b0bfc4d33403a311f56017d3d540ff819d
Jonathan Klaiber <jklaiber@oefenweb.nl>
2017-02-20 13:45:16 +0100

    initial commit of package oefenwebTools

---
+++ R/getItems.R
@@ -0,0 +1,82 @@
+#' Function to obtain items without JSON format and with tags (if available).
+#'
+#' @param domainId An item domain id
+#' @param itemStatus Item status (in numeric or character),
+#                   it is possible to enter severl states like this
+#                   for example: c(1, 2, 3).
+#' @return Item dataset with tags (if available) and without JSON formatting
+getItems <- function(domainId,
+                     itemStatus = 1) {
+  con <- oefenwebDatabase::connect()
+  domains <- suppressWarnings(DBI::dbReadTable(con, "domains"))
+  domainId <- suppressWarnings(as.numeric(domainId))
+
+  # check if domainId is valid
+  if (is.na(domainId)) {
+    message("The domain id must be a number object.")
+    return(NULL)
+  }
+  if (!(domainId %in% domains$id)) {
+    message("This item domain id does not exist.")
+    return(NULL)
+  }
+
+  # make status string if several states are requested
+  statusStr <- paste(itemStatus, collapse = ",")
+
+  items <- suppressWarnings(DBI::dbGetQuery(con,
+                                            paste("SELECT *
+                                                  FROM items
+                                                  WHERE domain_id = ",
+                                                  domainId,
+                                                  "AND status IN(",
+                                                  statusStr,
+                                                  ")")))
+
+  colnames(items)[which(colnames(items) == "id")] <- "item_id"
+  itemIds <- items$item_id
+  itemIdsPasted <- paste(itemIds, collapse =  ",")
+
+  itemTags <- suppressWarnings(DBI::dbGetQuery(con,
+                                               paste(" SELECT *
+                                                     FROM items_tags
+                                                     WHERE item_id IN (",
+                                                     itemIdsPasted, ")")))
+
+  if (nrow(itemTags) != 0) {
+    tagIdsPasted <- paste(itemTags$tag_id, collapse = ",")
+
+    tags <- suppressWarnings(DBI::dbGetQuery(con,
+                                             paste("SELECt *
+                                                   FROM tags
+                                                   WHERE id IN (",
+                                                   tagIdsPasted, ")")))
+    colnames(tags)[which(colnames(tags) == "id")] <- "tag_id"
+
+    tagNames <- plyr::join(itemTags, tags, by = "tag_id")[, c("item_id", "name")]
+
+    maxTagNr <- max(table(tagNames$item_id))
+
+    tagDF <- data.frame(matrix(NA, ncol = maxTagNr + 1, nrow = nrow(items)))
+    colnames(tagDF) <- c("item_id", paste0("tag", c(1:maxTagNr)))
+
+    for (i in 1:length(unique(tagNames$item_id))) {
+      tagDF$item_id[i] <- unique(tagNames$item_id)[i]
+      curTags <- tagNames$name[which(tagNames$item_id == unique(tagNames$item_id)[i])]
+      tagDF[i, c(2:(length(curTags) + 1))] <- curTags
+    }
+
+    itemsWithTags <- plyr::join(items, tagDF, by = "item_id")
+  } else {
+    message(paste0("There are no tags for items with domain id ", domainId,
+                   " (", domains$name[which(domains$id == domainId)], ")."))
+    itemsWithTags <- items
+  }
+
+  # close connnection
+  oefenwebDatabase::close_connections()
+  # remove JSON
+  parsedItems <- oefenwebItemJsonParser::itemJsonParser(itemsWithTags,
+                                                        withFeedback = TRUE)
+  return(parsedItems)
+}
---
+++ R/oefenwebTools.R
@@ -0,0 +1,16 @@
+#' oefenwebTools: A package that contains functionality related to Oefenweb
+#'
+#' The oefenwebTools package provides functionality related to Oefenweb tools. It
+#' contains for example a function called "getItems" which can be used to retrieve
+#' items from item database with attached tags.
+#'
+#'
+#' @section getItems function: The oefenwebTools package contains the function
+#' getItems which can be used to obtain items from the Oefenweb item database,
+#' attached tags to the items if available and to remove any JSON format in order to
+#' make item questions and answers readable. The only input variables for the getItems
+#' function are the domain id and status of items (optionally with default status = 1 (active))
+#'
+#' @docType package
+#' @name oefenwebTools
+NULL
